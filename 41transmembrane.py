159a32c993dbce54ea0451818e873f48b0096f5a
Cdvas <camvasquez@ucdavis.edu>
2023-03-21 13:08:12 -0700

    update

---
+++ 41transmembrane.py
@@ -0,0 +1,76 @@
+# 41transmembrane.py
+
+# Write a program that predicts which proteins in a proteome are transmembrane
+
+# Transmembrane proteins are characterized by having
+#    1. a hydrophobic signal peptide near the N-terminus
+#    2. other hydrophobic regions to cross the plasma membrane
+
+# Both the signal peptide and the transmembrane domains are alpha-helices
+# Therefore, they do not contain Proline
+
+# Hydrophobicity can be measured by Kyte-Dolittle
+#	https://en.wikipedia.org/wiki/Hydrophilicity_plot
+
+# For our purposes:
+#	Signal peptide is 8 aa long, KD > 2.5, first 30 aa
+#	Hydrophobic region is 11 aa long, KD > 2.0, after 30 aa
+
+# Hint: copy mcb185.py to your homework repo and import that
+# Hint: create a function for KD calculation
+# Hint: create a function for hydrophobic alpha-helix
+# Hint: use the same function for both signal peptide and transmembrane
+
+import mcb185
+import sys
+
+values = (4.5, 4.2, 3.8, 2.8, 2.5, 1.9, 1.8, -0.4, -0.7, -0.8, -0.9, -1.3, -1.6, -3.2, -3.5, -3.5, -3.5, -3.5, -3.9, -4.5) #A list of values from K-D wiki list
+
+aas = 'IVLFCMAGTSWYPHEQDNKR'
+
+def kd(data):
+	if hydrophobe == True: 
+		print(name)
+		
+hydrophobics = ['']
+		
+def hah(seq, window, threshold):#grab the kd values in each window
+	for i in range(len(seq) - window + 1):
+		kd = 0
+		hydrophobe = False
+		frame = seq[i:i+window]
+		if 'P' in frame: #doesn't process windows w/ P
+			continue
+		if name in hydrophobics:
+			continue
+		for aa in frame: 
+			idx = aas.find(aa)
+			kd += values[idx]
+		if kd > threshold: 
+			print(name)
+			hydrophobics.append(name)
+			break
+		
+		
+#		kd(frame) #keep track of when you exceed 2.5 or 2.0. Don't use return command bc that will end the loop after 1 iteration - Lilith
+		
+for name, seq in mcb185.read_fasta(sys.argv[1]): #make 1st 30aa and rest separately processed
+	roi1 = seq[:30]
+	roi2 = seq[30:]
+	hah(roi1, 11, 2.5)
+	hah(roi2, 8, 2.0)
+	
+"""
+python3 41transmembrane.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_protein.faa.gz
+NP_414560.1 Na(+):H(+) antiporter NhaA [Escherichia coli str. K-12 substr. MG1655]
+NP_414568.1 lipoprotein signal peptidase [Escherichia coli str. K-12 substr. MG1655]
+NP_414582.1 L-carnitine:gamma-butyrobetaine antiporter [Escherichia coli str. K-12 substr. MG1655]
+NP_414607.1 DedA family protein YabI [Escherichia coli str. K-12 substr. MG1655]
+NP_414609.1 thiamine ABC transporter membrane subunit [Escherichia coli str. K-12 substr. MG1655]
+NP_414653.1 protein AmpE [Escherichia coli str. K-12 substr. MG1655]
+NP_414666.1 quinoprotein glucose dehydrogenase [Escherichia coli str. K-12 substr. MG1655]
+NP_414695.1 iron(III) hydroxamate ABC transporter membrane subunit [Escherichia coli str. K-12 substr. MG1655]
+NP_414699.1 PF03458 family protein YadS [Escherichia coli str. K-12 substr. MG1655]
+NP_414717.2 CDP-diglyceride synthetase [Escherichia coli str. K-12 substr. MG1655]
+...
+"""
